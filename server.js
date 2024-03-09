6f2fbd170f9b12abd002e88c6ccfc8fdfbb6fdbd
pawu <pbarach555@rku.ac.in>
2024-03-09 18:15:23 +0530

    first commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app = express()
 const PORT = process.env.PORT || 5000
 
-mongoose.connect("mongodb+srv://paawanbarach:e4SSCy3HZLRrHSiB@clipboard.ykegl6h.mongodb.net/clipboard?retryWrites=true&w=majority&appName=Clipboard", {
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
